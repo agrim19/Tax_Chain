6cee0cf5c3c519ab6e731a4891def80b7281d22d
Agrim <agrim19229@iiitd.ac.in>
2021-05-06 21:56:32 +0530

    remove work unit tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,5 +11,4 @@ module.exports = async (deployer) => {
     await deployer.deploy(Transaction, KYC.address, GovtDetails.address);
     await deployer.deploy(GovtAllocate, GovtDetails.address);
     await deployer.deploy(Contractor);
-    await deployer.deploy(Work, Contractor.address);
 };
