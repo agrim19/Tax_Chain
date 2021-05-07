7d6b174ddd78f5df84f2777bdb31b7206ccc3c82
Aastha <aastha19224@iiitd.ac.in>
2021-05-07 21:46:57 +0530

    fix dots image postioning

--- client/src/pages/forms/login.js
+++ client/src/pages/forms/login.js
@@ -2,31 +2,36 @@ import React from "react";
 import "./form.css";
 
 function Login() {
-  return (
-	<div className="black-bg">
-	<div className="form-container">
-		<h1>Login Up</h1>
+    return (
+        <div className="auth-black-bg">
+            <div className="form-container">
+                <h1>Login Up</h1>
 
-		<form action="/login" method="POST">
-		<div className="input-field">
-			<i className="bx bxs-user"></i>
-			<input type="email" name="email" id="email" placeholder="Email" />
-		</div>
+                <form action="/login" method="POST">
+                    <div className="input-field">
+                        <i className="bx bxs-user"></i>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="Email"
+                        />
+                    </div>
 
-		<div className="input-field">
-			<i className="bx bxs-key"></i>
-			<input
-			type="password"
-			name="password"
-			id="password"
-			placeholder="password"
-			/>
-		</div>
-		<button className="btn-submit">Login!</button>
-		</form>
-	</div>
-	</div>
-  );
+                    <div className="input-field">
+                        <i className="bx bxs-key"></i>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="password"
+                        />
+                    </div>
+                    <button className="btn-submit">Login!</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
