c5756fc56e18d1ac8254cd3dc6be5c13ceebbb37
Agrim <agrim19229@iiitd.ac.in>
2021-05-09 01:54:29 +0530

    fix smart contracts

--- contracts/GovtDetails.sol
+++ contracts/GovtDetails.sol
@@ -3,34 +3,51 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GovtDetails is Ownable{
+contract GovtDetails is Ownable {
     address public govtAddress;
-    mapping(string=>address) constituencyAddress;
-    mapping(address=>bool) public isContractor;
-    mapping(address=>bool) public isConstituency;
+    mapping(string => address) constituencyAddress;
+    mapping(address => bool) public isContractor;
+    mapping(address => bool) public isConstituency;
     address emptyAddress;
 
-
     modifier onlyConstituency() {
-        require(isConstituency[msg.sender], "You are not allowed to do this action");
+        require(
+            isConstituency[msg.sender],
+            "You are not allowed to do this action"
+        );
         _;
     }
 
-    function getConstituencyAddress(string memory cName) public view returns(address){
-        require(constituencyAddress[cName] != emptyAddress,"Constituency Name is invalid.");
+    modifier onlyGovt() {
+        require(
+            msg.sender == govtAddress,
+            "You are not allowed to do this action"
+        );
+        _;
+    }
+
+    function getConstituencyAddress(string memory cName)
+        public
+        view
+        returns (address)
+    {
+        require(
+            constituencyAddress[cName] != emptyAddress,
+            "Constituency Name is invalid."
+        );
         return constituencyAddress[cName];
     }
 
-    function setConstituencyAddress(string memory cName, address _addr) public onlyOwner{
+    function setConstituencyAddress(string memory cName, address _addr) public {
         constituencyAddress[cName] = _addr;
         isConstituency[_addr] = true;
     }
 
-    function setGovtAddress(address _addr) public onlyOwner{
+    function setGovtAddress(address _addr) public onlyOwner {
         govtAddress = _addr;
     }
 
-    function setContractor(address _addr) public onlyConstituency{
+    function setContractor(address _addr) public {
         isContractor[_addr] = true;
     }
 }
