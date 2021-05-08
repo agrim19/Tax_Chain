ba3344cffcb50dd77b7475f1e18b1b65ff37da43
Agrim <agrim19229@iiitd.ac.in>
2021-05-08 21:59:28 +0530

    connect form for add constituency

--- client/src/components/addConstituency/addConstituency.js
+++ client/src/components/addConstituency/addConstituency.js
@@ -3,6 +3,8 @@ import { Col, Button, Form, Card } from 'react-bootstrap';
 import GovtDetails from '../../contracts/GovtDetails.json';
 
 function AddConstituency(web3) {
+    const [name, setName] = useState('');
+    const [addr, setAddr] = useState('');
     const handleSubmit = async (event) => {
         event.preventDefault();
         web3.web3.web3.eth.getAccounts().then(async (accounts) => {
@@ -14,7 +16,7 @@ function AddConstituency(web3) {
                 contractAddress
             );
             contract.methods
-                .setConstituencyAddress('delhi', account)
+                .setConstituencyAddress(name.toLowerCase(), addr)
                 .send({ from: account })
                 .then((result) => {
                     console.log(result);
@@ -31,9 +33,9 @@ function AddConstituency(web3) {
                         <Form.Control
                             type='text'
                             placeholder='Constituency Name'
-                            // onChange={(event) => {
-                            //     setToAccount(event.target.value);
-                            // }}
+                            onChange={(event) => {
+                                setName(event.target.value);
+                            }}
                         />
                     </Form.Group>
 
@@ -42,7 +44,7 @@ function AddConstituency(web3) {
                             type='text'
                             placeholder='Constituency Address'
                             onChange={(event) => {
-                                // setToValue(event.target.value);
+                                setAddr(event.target.value);
                             }}
                         />
                     </Form.Group>
