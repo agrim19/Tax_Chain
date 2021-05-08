22b896211a83b7b95ea649a38ff1d5b3412e4829
Agrim <agrim19229@iiitd.ac.in>
2021-05-09 00:45:53 +0530

    firebase linking: listen to pay tax event

--- client/src/pages/citizen/citizen.js
+++ client/src/pages/citizen/citizen.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../../components/footer/footer.js';
 import NavigationBar from '../../components/navbar/navbar.js';
 import Header from '../../components/header/header.js';
-import { Row, Col, Button, Form, Card, Container } from 'react-bootstrap';
+import {
+    Row,
+    Col,
+    Button,
+    Form,
+    Card,
+    Container,
+    Alert,
+} from 'react-bootstrap';
 import Transactions from '../../components/transactions/transactions.js';
 import CurrentTokens from '../../components/currentTokens/currentTokens.js';
 import PurchaseTokens from '../../components/purchaseTokens/purchaseTokens.js';
@@ -13,34 +21,69 @@ import './citizen.css';
 
 export default function Citizen({ web3 }) {
     const { currentUser } = useAuth();
+    const [account, setAccount] = useState('');
+    const [networkId, setNetworkId] = useState('');
+    const [transactContract, setTransactContract] = useState('');
+    const [mineMessage, setMineMessage] = useState('');
     var description =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam sollicitudin tempor id eu nisl nunc. Malesuada bibendum arcu vitae elementum curabitur vitae. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Nisl vel pretium lectus quam id leo in vitae turpis. A lacus vestibulum sed arcu non odio euismod. Tincidunt vitae semper quis lectus nulla. Pulvinar elementum integer enim neque volutpat ac. Tortor at risus viverra adipiscing at. Placerat in egestas erat imperdiet sed. Turpis tincidunt id aliquet risus. Sed enim ut sem viverra. Ultricies tristique nulla aliquet enim tortor at auctor urna nunc.';
+    useEffect(() => {
+        web3.eth.getAccounts().then(async (accounts) => {
+            setAccount(accounts[0]);
+            web3.eth.net.getId().then((result) => {
+                setNetworkId(result);
+                let contractAddress = Transact.networks[result].address;
+                let TransactContract = new web3.eth.Contract(
+                    Transact.abi,
+                    contractAddress
+                );
+                setTransactContract(TransactContract);
+                TransactContract.events
+                    .TaxPaid({})
+                    .on('data', async (event) => {
+                        let values = event.returnValues;
+                        // values = (_who, taxBefore, taxAfter);
+                        db.collection('tax-payments')
+                            .add({
+                                amount: values.taxAfter - values.taxBefore,
+                                date: new Date().toLocaleDateString('en-US'),
+                                payerAddress: values._who,
+                            })
+                            .then((docRef) => {
+                                console.log(
+                                    'Document written with ID: ',
+                                    docRef.id
+                                );
+                            })
+                            .catch((error) => {
+                                console.error(
+                                    'Error writing document: ',
+                                    error
+                                );
+                                // setError('Error writing document:');
+                            });
+                    });
+            });
+        });
+    }, []);
     const handlePayTax = async (event) => {
         event.preventDefault();
         let userBlockChainAddress = await getBlockChainAddress(
             currentUser.email
         );
-        web3.eth.getAccounts().then(async (accounts) => {
-            let account = accounts[0];
-            if (account.toLowerCase() != userBlockChainAddress.toLowerCase()) {
-                alert('NOT your account');
-                return;
-            }
-            let networkId = await web3.eth.net.getId();
-            let contractAddress = Transact.networks[networkId].address;
-            let transactContract = new web3.eth.Contract(
-                Transact.abi,
-                contractAddress
-            );
-            transactContract.methods
-                .payTax(1)
-                .send({ from: account })
-                .then((result) => {
-                    console.log(result);
-                    alert('Done');
-                })
-                .catch(alert);
-        });
+        if (account.toLowerCase() != userBlockChainAddress.toLowerCase()) {
+            alert('NOT your account');
+            return;
+        }
+        transactContract.methods
+            .payTax(1)
+            .send({ from: account })
+            .then((result) => {
+                console.log(result);
+                alert('Done');
+                setMineMessage('Transaction Completed');
+            })
+            .catch(alert);
     };
     return (
         <div className=''>
@@ -68,6 +111,17 @@ export default function Citizen({ web3 }) {
                             <Card className='table-card'>
                                 <Card.Body>
                                     <h2>Pay Tax</h2>
+                                    {mineMessage ? (
+                                        <Alert
+                                            variant='success'
+                                            className='mb-5'
+                                        >
+                                            {mineMessage}
+                                        </Alert>
+                                    ) : (
+                                        ''
+                                    )}
+
                                     <Form>
                                         <Form.Row>
                                             <Form.Group as={Col}>
