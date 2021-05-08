f34928758d33c924bb6074ba03b34b3db34b3f0d
Agrim <agrim19229@iiitd.ac.in>
2021-05-08 20:33:31 +0530

    add functionality to add contractor button

--- client/src/pages/constituency/consituency.js
+++ client/src/pages/constituency/consituency.js
@@ -1,86 +1,87 @@
-import React from "react";
-import { Row, Col, Button, Form, Card, Container } from "react-bootstrap";
-import NavigationBar from "../../components/navbar/navbar.js";
-import Footer from "../../components/footer/footer.js";
-import Header from "../../components/header/header.js";
-import Fund from "../../components/fund/fund.js";
-import TransferFunds from "../../components/transferFund/transferFund.js";
-import Transactions from "../../components/transactions/transactions.js";
-import Kyc from "../../components/kyc/kyc.js";
+import React from 'react';
+import { Row, Col, Button, Form, Card, Container } from 'react-bootstrap';
+import NavigationBar from '../../components/navbar/navbar.js';
+import Footer from '../../components/footer/footer.js';
+import Header from '../../components/header/header.js';
+import Fund from '../../components/fund/fund.js';
+import TransferFunds from '../../components/transferFund/transferFund.js';
+import Transactions from '../../components/transactions/transactions.js';
+import Kyc from '../../components/kyc/kyc.js';
+import GovtDetails from '../../contracts/GovtDetails.json';
 
-import "./constituency.css";
+import './constituency.css';
 
 function Constituency(web3) {
-  return (
-    <div>
-      <NavigationBar></NavigationBar>
-      <Header heading="Malviya Nagar Constituency"></Header>
-      <Container className="my-5">
-        <Row>
-          <Fund name="Alloted Funds" value="2392138"></Fund>
-          <Fund name="Used Funds" value="3202"></Fund>
-        </Row>
-        <Row>
-          <Transactions heading="Transactions in Your Constituency"></Transactions>
-          <TransferFunds></TransferFunds>
-          <Kyc web3={web3}></Kyc>
-        </Row>
-      </Container>
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        web3.web3.eth.getAccounts().then(async (accounts) => {
+            let account = accounts[0];
+            let networkId = await web3.web3.eth.net.getId();
+            let contractAddress = GovtDetails.networks[networkId].address;
+            let contract = new web3.web3.eth.Contract(
+                GovtDetails.abi,
+                contractAddress
+            );
+            contract.methods
+                .setContractor(account)
+                .send({ from: account })
+                .then((result) => {
+                    console.log(result);
+                    alert('Successful');
+                })
+                .catch(alert);
+        });
+    };
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+            <Header heading='Malviya Nagar Constituency'></Header>
+            <Container className='my-5'>
+                <Row>
+                    <Fund name='Alloted Funds' value='2392138'></Fund>
+                    <Fund name='Used Funds' value='3202'></Fund>
+                </Row>
+                <Row>
+                    <Transactions heading='Transactions in Your Constituency'></Transactions>
+                    <TransferFunds></TransferFunds>
+                    <Kyc web3={web3}></Kyc>
+                </Row>
 
-      {/* form below */}
+                {/* form below */}
 
-      <div>
-        <Card className="table-card">
-          <Card.Body>
-            <h2>Pay Tax</h2>
-            <Form>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>PAN No</Form.Label>
-                  <Form.Control type="text" placeholder="PAN No" />
-                </Form.Group>
-              </Form.Row>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
-                </Form.Group>
+                <div>
+                    <Card className='table-card'>
+                        <Card.Body>
+                            <h2>Add Contractor</h2>
+                            <Form>
+                                <Form.Row>
+                                    <Form.Group as={Col}>
+                                        <Form.Label></Form.Label>
+                                        <Form.Control
+                                            type='text'
+                                            placeholder='0x0000000000000000'
+                                        />
+                                    </Form.Group>
+                                </Form.Row>
+                                <Button
+                                    type='submit'
+                                    className='constituency-find-btn'
+                                    onClick={handleSubmit}
+                                >
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Container>
 
-                <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control as="select" defaultValue="Choose...">
-                    <option>State</option>
-                    <option>Delhi</option>
-                    <option>Haryana</option>
-                    <option>Rajasthan</option>
-                  </Form.Control>
-                </Form.Group>
+            {/* form above */}
 
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-              </Form.Row>
-              <Button type="submit" className="constituency-find-btn">
-                Submit
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </div>
-
-      {/* form above */}
-
-      <div className="constituency-above-footer"></div>
-      <Footer></Footer>
-    </div>
-  );
+            <div className='constituency-above-footer'></div>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default Constituency;
--- contracts/GovtDetails.sol
+++ contracts/GovtDetails.sol
@@ -7,7 +7,14 @@ contract GovtDetails is Ownable{
     address public govtAddress;
     mapping(string=>address) constituencyAddress;
     mapping(address=>bool) public isContractor;
+    mapping(address=>bool) public isConstituency;
     address emptyAddress;
+
+
+    modifier onlyConstituency() {
+        require(isConstituency[msg.sender], "You are not allowed to do this action");
+        _;
+    }
 
     function getConstituencyAddress(string memory cName) public view returns(address){
         require(constituencyAddress[cName] != emptyAddress,"Constituency Name is invalid.");
@@ -16,13 +23,14 @@ contract GovtDetails is Ownable{
 
     function setConstituencyAddress(string memory cName, address _addr) public onlyOwner{
         constituencyAddress[cName] = _addr;
+        isConstituency[_addr] = true;
     }
 
     function setGovtAddress(address _addr) public onlyOwner{
         govtAddress = _addr;
     }
 
-    function setContractor(address _addr) public onlyOwner{
+    function setContractor(address _addr) public onlyConstituency{
         isContractor[_addr] = true;
     }
 }
